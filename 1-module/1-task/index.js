770849ce4902c9e6b76ac30e871b78e57e1f9337
Evgenii <osipenkov.evgenii@gmail.com>
2019-11-05 22:14:33 +0300

    Решение задачи из второго модуля

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,14 @@
  * @returns {number}
  */
 function pow(m, n) {
-
+  let result = m;
+  for (let i = 1; i < n; i++) {
+    result *= m;
+  }
+  return result;
 }
+
+let m = +prompt('Введите число:', '');
+let n = +prompt('Введите натуральную степень:', '');
+
+alert(pow(m, n));
