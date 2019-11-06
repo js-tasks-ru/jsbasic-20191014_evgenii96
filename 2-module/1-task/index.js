6a64db26a3e397f78399a169cd9e90e55823a9bc
Evgenii <osipenkov.evgenii@gmail.com>
2019-11-06 21:18:48 +0300

    Решение первой задачи Модуля 2 - клонирование объекта

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,32 @@
  * @returns {Object}
  */
 function clone(obj) {
+  let cloneObj = {};
+  for (let key in obj) {
+    if (obj[key] === null) {
+      cloneObj[key] = null;
+    } else if (typeof (obj[key]) === 'object') {
+      cloneObj[key] = clone(obj[key]);
+    } else {
+      cloneObj[key] = obj[key];
+    }
+  }
+  return cloneObj;
 }
+
+const obj = {
+  prop: 1,
+  anotherProp: 2,
+  method () {
+    alert(this.prop);
+  },
+  anotherObj: {},
+};
+
+cloneObj = clone(obj);
+
+// console.log(cloneObj === obj);
+// console.log(cloneObj.anotherObj === obj.anotherObj);
+// alert(cloneObj.anotherObj);
+// alert(obj.anotherObj);
+
