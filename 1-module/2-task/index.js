a918b59ac55f8bb9a457dd3558b904836cb1d359
Evgenii <osipenkov.evgenii@gmail.com>
2019-11-06 21:36:55 +0300

    Решение задачи на валидацию имени пользователя

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,14 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (!name) {
+    return false;
+  } else if (name.length < 4) {
+    return false;
+  } else if (name.includes(' ')) {
+    return false;
+  }
+  return true;
 }
 
 function sayHello() {
