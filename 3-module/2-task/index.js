42bf4d90e320255120d0e670128052dbaab280e3
Evgenii <osipenkov.evgenii@gmail.com>
2019-11-12 21:17:29 +0300

    Решение задачи 3.2 Найти min/max в строке

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,12 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(string) {
+  let values = string.split(' ')
+                      .join(',')
+                      .split(',')
+                      .map((item) => parseFloat(item))
+                      .filter((item) => !isNaN(item))
+                      .sort(function(a, b) {return a - b;});
+  return {min: values[0], max: values[values.length - 1]};
 }
